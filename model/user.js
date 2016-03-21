670dce84d054e9850c571eb57af1c76653b610b5
wallaceyuan <1272864289@qq.com>
2016-03-21 22:27:25 +0800

    reg

--- model/user.js
+++ model/user.js
@@ -8,7 +8,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/yuanblog');
 //定义模型 确定数据库里表结构
 var userSchema = new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    email:String,
+    avatar:String
 });
 //再定义model
 var userModel = mongoose.model('user',userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var userModel = require('../model/user');
 var middleware = require('../middleware/index');
+var crypto = require('crypto');
 var router = express.Router();
 
 /* GET users listing. */
@@ -10,12 +11,19 @@ router.get('/reg',middleware.checkNotLogin,function(req,res){
 //提交用户注册的表单
 router.post('/reg',middleware.checkNotLogin,function(req,res){
     var user = req.body;
+    if(user.password != user.repassword){
+        req.flash('error','两次输入的密码不一致');
+        return res.redirect('/users/reg');
+    }
+    delete user.repassword;
+    user.password = md5(user.password);
+    user.avatar = "https://secure.gravatar.com/avatar/"+md5(user.email)+"?s=48";
     userModel.create(user,function(err,doc){
         if(err){
-            req.flash('error', '登录失败!');
+            req.flash('error', '注册失败!');
             res.redirect('back');//返回到上一个页面
         }else{
-            req.flash('success', '登录成功!');
+            req.flash('success', '注册成功!');
             req.session.user = doc;
             res.redirect('/');
         }
@@ -29,13 +37,27 @@ router.get('/login',middleware.checkNotLogin,function(req,res){
 
 //提交用户登录的表单
 router.post('/login',middleware.checkNotLogin,function(req,res){
-    res.send('login');
+    var user = req.body;
+    user.password = md5(user.password);
+    userModel.findOne(user,function(err,doc){
+        console.log(doc);
+        if(err ||doc == null){
+            req.flash('error', '登录失败!');
+            res.redirect('/users/login');
+        }else{
+            req.flash('success', '登录成功!');
+            req.session.user = user;//用户信息存入 session
+            res.redirect('/');//注册成功后返回主页
+        }
+    });
 });
 
 //退出登录
 router.get('/logout',middleware.checkLogin,function(req,res){
     req.session.user  = null;
-    res.send('logout');
+    res.redirect('/');//注册成功后返回主页
 });
-
+function md5(val){
+    return crypto.createHash('md5').update(val).digest('hex');
+}
 module.exports = router;
